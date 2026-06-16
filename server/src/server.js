70b91c14bec3cb554651a336ce8429d6de0d3205
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-06-16 23:34:56 +0530

    setup: client and server configured

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/',(req, res)=>{
+    res.send("Hello world from backend")
+})
+
+
+app.listen(3000, ()=>{
+    console.log("Server is running on 3000");
+    
+})
