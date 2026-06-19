53f74024ec65ce2b1a2dc7feef3c6ead56597bf8
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-06-19 15:33:03 +0530

    baceknd: server configuration

--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
-import { connectDB } from './src/config/db.js'
-
+import connectDB from './config/db.js'
 dotenv.config()
 
+// configuration
 const app = express()
+const PORT = process.env.PORT || 5000
 
+// built in middlewares
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json())
 
+// health checker routes
 app.get('/health', (req, res) => res.json({ status: 'ok' }))
 
-const PORT = process.env.PORT || 5000
-
+// database connection
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 })
